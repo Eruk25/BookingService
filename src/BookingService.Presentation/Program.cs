7e02e0ea9ce321ded374c7e9a45f96306e5bac50
Danila <danilamatush72@gmail.com>
2025-09-02 16:42:55 +0300

    feat(presentation): registration ApplicationContext in DI

--- src/BookingService.Presentation/Program.cs
+++ src/BookingService.Presentation/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDBContext<ApplicationContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
