f2c804fdc373ecb15060da9478092cc81124b619
Danila <danilamatush72@gmail.com>
2025-09-02 15:54:20 +0300

    feat(infrastructure): add ApplicationContext

---
+++ src/BookingService.Infrustructure/Persistance/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using BookingService.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingService.Infrastructure;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Resource> Resources { get; set; }
+}
