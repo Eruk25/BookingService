96a65bee8e3d10fc09836224ed3c8bb4779372cb
Danila <danilamatush72@gmail.com>
2025-09-02 12:09:10 +0300

    feat(domain): add Resource entity

--- src/BookingService.Domain/Entities/Resource.cs
+++ src/BookingService.Domain/Entities/Resource.cs
@@ -2,5 +2,9 @@
 
 public class Resource
 {
-
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal PricePerHour { get; set; }
+    public string ImageUrl { get; set; }
 }
