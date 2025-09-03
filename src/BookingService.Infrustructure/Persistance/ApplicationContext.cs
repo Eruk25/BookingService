a1e4dab3e55082375985c8c1d496099f530c749b
Danila <danilamatush72@gmail.com>
2025-09-03 15:44:59 +0300

    feat(infrastructure): add configuration for User model in ApplicationContext

--- src/BookingService.Infrustructure/Persistance/ApplicationContext.cs
+++ src/BookingService.Infrustructure/Persistance/ApplicationContext.cs
@@ -5,12 +5,31 @@ namespace BookingService.Infrastructure;
 
 public class ApplicationContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Resource> Resources { get; set; }
     public ApplicationContext(DbContextOptions<ApplicationContext> options)
         : base(options)
     {
     }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Booking> Bookings { get; set; }
-    public DbSet<Resource> Resources { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(u => u.Id);
+        modelBuilder.Entity<User>()
+            .Property(u => u.UserName)
+            .IsRequired()
+            .HasMaxLength(50);
+        modelBuilder.Entity<User>()
+            .Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(255);
+        modelBuilder.Entity<User>()
+            .Property(u => u.Password)
+            .IsRequired()
+            .HasMaxLength(30);
+        modelBuilder.Entity<User>().HasIndex(u => u.UserName).IsUnique();
+        modelBuilder.Entity<User>().HasIndex(u =>  u.Email).IsUnique();
+    }
 }
     
