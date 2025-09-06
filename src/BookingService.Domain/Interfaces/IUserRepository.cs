b2455027df0fd7d9defa5b1d4dbc391dd4d39db2
Danila <danilamatush72@gmail.com>
2025-09-06 14:58:49 +0300

    feat(infrastructure): add EF implementation for IUserRepository

--- src/BookingService.Domain/Interfaces/IUserRepository.cs
+++ src/BookingService.Domain/Interfaces/IUserRepository.cs
@@ -5,8 +5,8 @@ namespace BookingService.Domain.Interfaces;
 public interface IUserRepository
 {
     Task<IEnumerable<User>> GetAllAsync();
-    Task<User> GetByIdAsync(int id);
-    Task<User> GetByEmailAsync(string email);
+    Task<User?> GetByIdAsync(int id);
+    Task<User?> GetByEmailAsync(string email);
     Task CreateAsync(User user);
     Task UpdateAsync(User user);
     Task DeleteAsync(int id);
---
+++ src/BookingService.Infrustructure/Persistance/Repositories/UserRepository.cs
@@ -0,0 +1,54 @@
+using BookingService.Domain.Entities;
+using BookingService.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingService.Infrastructure.Persistance.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationContext _context;
+
+    UserRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users.
+            FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task CreateAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        var entity = await _context.Users.FindAsync(user.Id);
+        entity.UserName = user.UserName;
+        entity.Email = user.Email;
+        entity.Password = user.Password;
+        
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+    }
+}
