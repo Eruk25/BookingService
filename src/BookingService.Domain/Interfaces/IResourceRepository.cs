d6dd76576a98fb240eb3f07147a104ce8d1ac652
Danila <danilamatush72@gmail.com>
2025-09-06 14:45:57 +0300

    feat(infrastructure): add EF implementation for IResourceRepository

--- src/BookingService.Domain/Interfaces/IResourceRepository.cs
+++ src/BookingService.Domain/Interfaces/IResourceRepository.cs
@@ -5,7 +5,7 @@ namespace BookingService.Domain.Interfaces;
 public interface IResourceRepository
 {
     Task<IEnumerable<Resource>> GetAllAsync();
-    Task<Resource> GetByIdAsync(int id);
+    Task<Resource?> GetByIdAsync(int id);
     Task CreateAsync(Resource resource);
     Task UpdateAsync(Resource resource);
     Task DeleteAsync(int id);
---
+++ src/BookingService.Infrustructure/Persistance/Repositories/ResourceRepository.cs
@@ -0,0 +1,49 @@
+using BookingService.Domain.Entities;
+using BookingService.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingService.Infrastructure.Persistance.Repositories;
+
+public class ResourceRepository : IResourceRepository
+{
+    private readonly ApplicationContext _context;
+
+    ResourceRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<Resource>> GetAllAsync()
+    {
+        return await _context.Resources.ToListAsync();
+    }
+
+    public async Task<Resource?> GetByIdAsync(int id)
+    {
+        return await _context.Resources.FindAsync(id);
+    }
+
+    public async Task CreateAsync(Resource resource)
+    {
+        await _context.Resources.AddAsync(resource);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Resource resource)
+    {
+        var entity = await _context.Resources.FindAsync(resource.Id);
+        entity.Title = resource.Title;
+        entity.Description = resource.Description;
+        entity.PricePerHour = resource.PricePerHour;
+        entity.ImageUrl = resource.ImageUrl;
+        
+        await _context.SaveChangesAsync();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        var entity = _context.Resources.Find(id);
+        _context.Resources.Remove(entity);
+        return _context.SaveChangesAsync();
+    }
+}
