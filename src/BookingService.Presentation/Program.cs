3ad8614d6003c5acc9441c046d98d02c0b771bce
Danila <danilamatush72@gmail.com>
2025-09-06 15:04:30 +0300

    feat(presentation): register repositories in DI

--- src/BookingService.Presentation/Program.cs
+++ src/BookingService.Presentation/Program.cs
@@ -1,5 +1,7 @@
+using BookingService.Domain.Interfaces;
 using BookingService.Infrastructure;
 using BookingService.Infrastructure.Persistance;
+using BookingService.Infrastructure.Persistance.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
