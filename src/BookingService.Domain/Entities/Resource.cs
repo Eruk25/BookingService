5bfaa259c2f0a2e1a797df026e92cb4cce20cf5d
Danila <danilamatush72@gmail.com>
2025-09-08 09:38:51 +0300

    feat(domain): add base validation for Resource model

--- src/BookingService.Domain/Entities/Resource.cs
+++ src/BookingService.Domain/Entities/Resource.cs
@@ -7,4 +7,21 @@ public class Resource
     public string Description { get; set; }
     public decimal PricePerHour { get; set; }
     public string ImageUrl { get; set; }
+
+    public Resource(string title, string description,
+        decimal pricePerHour, string imageUrl)
+    {
+        if(string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be null or empty", nameof(title));
+        if(string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException("Description cannot be null or empty", nameof(description));
+        if(pricePerHour <= 0)
+            throw new ArgumentException("Price per hour cannot be greater than zero", nameof(pricePerHour));
+        if(string.IsNullOrWhiteSpace(imageUrl))
+            throw new ArgumentException("ImageUrl cannot be null or empty", nameof(imageUrl));
+        Title = title;
+        Description = description;
+        PricePerHour = pricePerHour;
+        ImageUrl = imageUrl;
+    }
 }
