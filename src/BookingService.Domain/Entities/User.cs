ca53a19b45542e0e790fd8a25cd8d3911c3dade0
Danila <danilamatush72@gmail.com>
2025-09-08 09:26:15 +0300

    feat(domain): add base validation for User model

--- src/BookingService.Domain/Entities/User.cs
+++ src/BookingService.Domain/Entities/User.cs
@@ -10,4 +10,19 @@ public class User
     public string Password { get; set; }
     public EnumRole Role { get; set; }
     public DateTime RegistrationDate { get; set; }
+
+    public User(string userName, string email, string password, EnumRole role)
+    {
+        if(string.IsNullOrWhiteSpace(userName))
+            throw new ArgumentException("User name cannot be null or empty", nameof(userName));
+        if(string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException("Email cannot be null or empty", nameof(email));
+        if(string.IsNullOrWhiteSpace(password))
+            throw new ArgumentException("Password cannot be null or empty", nameof(password));
+        UserName = userName;
+        Email = email;
+        Password = password;
+        Role = role;
+        RegistrationDate = DateTime.UtcNow;
+    }
 }
