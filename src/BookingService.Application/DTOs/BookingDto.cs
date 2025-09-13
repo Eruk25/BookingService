9224618469f0b7385752645a09602fc5a181a935
Danila <danilamatush72@gmail.com>
2025-09-13 13:31:42 +0300

    feat(application): change Resources to Resource in BookingDto

--- src/BookingService.Application/DTOs/BookingDto.cs
+++ src/BookingService.Application/DTOs/BookingDto.cs
@@ -3,7 +3,7 @@ namespace BookingService.Application.DTOs;
 public class BookingDto
 {
     public int BookingNumber { get; set; }
-    public List<ResourceDto> Resources { get; set; } = new();
+    public ResourceDto Resource { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
 }
