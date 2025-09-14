d099071d80f1b33f6dc318681810029957acc0ff
Danila <danilamatush72@gmail.com>
2025-09-14 15:38:37 +0300

    feat(presentation): add ResourceController with CRUD endpoints

--- src/BookingService.Presentation/Controllers/ResourceController.cs
+++ src/BookingService.Presentation/Controllers/ResourceController.cs
@@ -1,6 +1,52 @@
+using BookingService.Application.DTOs;
+using BookingService.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
 namespace BookingService.Presentation.Controllers;
 
-public class ResourceController
+[ApiController]
+[Route("api/[controller]")]
+public class ResourceController: ControllerBase
 {
-    
+    private readonly IResourceService _resourceService;
+
+    public ResourceController(IResourceService resourceService)
+    {
+        _resourceService = resourceService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ResourceDto>>> GetAllAsync()
+    {
+        var resources = await _resourceService.GetAllAsync();
+        return Ok(resources);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ResourceDto>> GetByIdAsync(int id)
+    {
+        var resource = await _resourceService.GetByIdAsync(id);
+        return Ok(resource);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ResourceDto>> CreateAsync(ResourceDto resource)
+    {
+        var createdResource = await _resourceService.CreateAsync(resource);
+        return CreatedAtAction(nameof(GetByIdAsync), createdResource);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<bool>> UpdateAsync(int id, ResourceDto resource)
+    {
+        var success = await _resourceService.UpdateAsync(resource);
+        return Ok(success);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<bool>> DeleteAsync(int id)
+    {
+        var success = await _resourceService.DeleteAsync(id);
+        return Ok(success);
+    }
 }
