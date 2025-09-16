4e5b6efad1b4f8fa2fa1f26d5e4a81218ce14fe0
Danila <danilamatush72@gmail.com>
2025-09-16 12:53:14 +0300

    feat(presentation): add field ImageUrl to ResourceDto

--- src/BookingService.Application/DTOs/ResourceDto.cs
+++ src/BookingService.Application/DTOs/ResourceDto.cs
@@ -6,4 +6,5 @@ public class ResourceDto
     public string Title { get; set; }
     public string Description { get; set; }
     public decimal PricePerHour { get; set; }
+    public string ImageUrl { get; set; }
 }
