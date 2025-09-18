3418b631c685d79b597b326b331133f2aa3d0715
Danila <danilamatush72@gmail.com>
2025-09-18 16:41:37 +0300

    feat(domain): change returned value in CreateAsync method

--- src/BookingService.Domain/Interfaces/IUserRepository.cs
+++ src/BookingService.Domain/Interfaces/IUserRepository.cs
@@ -7,7 +7,7 @@ public interface IUserRepository
     Task<IEnumerable<User>> GetAllAsync();
     Task<User?> GetByIdAsync(int id);
     Task<User?> GetByEmailAsync(string email);
-    Task CreateAsync(User user);
+    Task<bool> CreateAsync(User user);
     Task UpdateAsync(User user);
     Task DeleteAsync(int id);
 }
