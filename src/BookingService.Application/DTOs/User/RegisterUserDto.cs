6de8c10d1b63e10a0842c71cf504ef5120ad88b6
Danila <danilamatush72@gmail.com>
2025-09-18 16:12:10 +0300

    feat(application): add RegistersUserDto to Application

---
+++ src/BookingService.Application/DTOs/User/RegisterUserDto.cs
@@ -0,0 +1,8 @@
+namespace BookingService.Application.DTOs;
+
+public class RegisterUserDto
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
