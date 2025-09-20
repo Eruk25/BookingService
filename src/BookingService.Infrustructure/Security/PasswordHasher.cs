7092121e1ec6fdc18a3658557fb2f968b113f8b2
Danila <danilamatush72@gmail.com>
2025-09-20 16:25:25 +0300

    feat(application): implement IPasswordHasher in PasswordHasher

--- src/BookingService.Infrustructure/Security/PasswordHasher.cs
+++ src/BookingService.Infrustructure/Security/PasswordHasher.cs
@@ -1,6 +1,17 @@
+using BookingService.Application.Interfaces.PasswordHasher;
+
 namespace BookingService.Infrastructure.Security;
 
-public class PasswordHasher
+public class PasswordHasher :  IPassworHasher
 {
-    
+    private readonly Identity.PasswordHasher.PasswordHasher _hasher = new();
+    public string HashPassword(string password)
+    {
+        return _hasher.HashPassword(password);
+    }
+
+    public bool VerifyHashedPassword(string password, string hashedPassword)
+    {
+        return _hasher.VerifyHashedPassword(hashedPassword, password);
+    }
 }
