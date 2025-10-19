49c902f250d7346e2f51f31e2c00a45772b5e958
Danila <danilamatush72@gmail.com>
2025-10-19 15:56:28 +0300

    feat(domain): add ImportDataAsync method in IResourceRepository

--- src/BookingService.Domain/Interfaces/IResourceRepository.cs
+++ src/BookingService.Domain/Interfaces/IResourceRepository.cs
@@ -7,6 +7,7 @@ public interface IResourceRepository
     Task<IEnumerable<Resource>> GetAllAsync();
     Task<Resource?> GetByIdAsync(int id);
     Task CreateAsync(Resource resource);
+    Task ImportDataAsync(IEnumerable<Resource> resources);
     Task<bool> UpdateAsync(Resource resource);
     Task<bool> DeleteAsync(int id);
 }
