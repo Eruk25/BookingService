1bf0121347c8991c5f6b9045e6dd60b4738ef05e
Danila <danilamatush72@gmail.com>
2025-10-19 14:10:05 +0300

    test(parser): test parser work

---
+++ src/BookingService.Parser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using AngleSharp;
+using AngleSharp.Dom;
+using BookingService.Domain.Entities;
+using BookingService.Parser.Implementations;
+
+namespace BookingService.Parser
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+           KufarParser parser = new KufarParser();
+           var resources = await parser.ParseAsync();
+        }
+    }    
+}       
