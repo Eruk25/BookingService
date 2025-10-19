6385cbd82dc40f58b88fec5bd1d63ea504874d69
Danila <danilamatush72@gmail.com>
2025-10-19 16:00:12 +0300

    feat(application): add new fields Address, SourceId and change PricePerHour to PricePerMonth

--- src/BookingService.Application/DTOs/ResourceDto.cs
+++ src/BookingService.Application/DTOs/ResourceDto.cs
@@ -4,7 +4,9 @@ public class ResourceDto
 {
     public int ResourceNumber { get; set; }
     public string Title { get; set; }
+    public string Address { get; set; }
     public string Description { get; set; }
-    public decimal PricePerHour { get; set; }
+    public decimal PricePerMonth { get; set; }
     public string ImageUrl { get; set; }
+    public string SourceId { get; set; }
 }
