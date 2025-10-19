c821c0b43ef2359902f6e8d42793636e1f5a8c66
Danila <danilamatush72@gmail.com>
2025-10-19 14:05:43 +0300

    feat(domain): add field Address and SourceI, change name PricePerHour to PricePerMonth

--- src/BookingService.Domain/Entities/Resource.cs
+++ src/BookingService.Domain/Entities/Resource.cs
@@ -4,25 +4,31 @@ public class Resource
 {
     public int Id { get; set; }
     public string Title { get; set; }
+    public string Address { get; set; }
     public string Description { get; set; }
-    public decimal PricePerHour { get; set; }
+    public decimal PricePerMonth { get; set; }
     public string ImageUrl { get; set; }
+    public string SourceId { get; set; }
 
-    public Resource(string title, string description,
-        decimal pricePerHour, string imageUrl)
+    public Resource(string title, string description, string address
+        ,decimal pricePerMonth, string imageUrl, string sourceId)
     {
         if(string.IsNullOrWhiteSpace(title))
             throw new ArgumentException("Title cannot be null or empty", nameof(title));
         if(string.IsNullOrWhiteSpace(description))
             throw new ArgumentException("Description cannot be null or empty", nameof(description));
-        if(pricePerHour <= 0)
-            throw new ArgumentException("Price per hour cannot be greater than zero", nameof(pricePerHour));
+        if(string.IsNullOrWhiteSpace(address))
+            throw new ArgumentException("Address cannot be null or empty", nameof(address));
+        if(pricePerMonth <= 0)
+            throw new ArgumentException("Price per hour cannot be greater than zero", nameof(pricePerMonth));
         if(string.IsNullOrWhiteSpace(imageUrl))
             throw new ArgumentException("ImageUrl cannot be null or empty", nameof(imageUrl));
         Title = title;
         Description = description;
-        PricePerHour = pricePerHour;
+        Address = address;
+        PricePerMonth = pricePerMonth;
         ImageUrl = imageUrl;
+        SourceId = sourceId;
     }
-    protected Resource() {}
+    public Resource() {}
 }
