71f402aa49c5ad9e3479372b8cce5e67ef347127
Danila <danilamatush72@gmail.com>
2025-10-19 14:02:19 +0300

    feat(parser): add interfase IParser

--- src/BookingService.Parser/Abstractions/IParser.cs
+++ src/BookingService.Parser/Abstractions/IParser.cs
@@ -1,6 +1,14 @@
+using AngleSharp.Dom;
+using BookingService.Domain.Entities;
+
 namespace BookingService.Parser.Abstractions;
 
 public interface IParser
 {
-    
+    public Task<IDocument?> GetDocumentAsync(string? link = null);
+    public IEnumerable<string> GetPaginationLinks(IDocument document);
+    public IEnumerable<IElement>? GetCards(IDocument document);
+    public Task<IEnumerable<Resource>> ParseAsync();
+    public Resource? ParseCard(IElement card);
+    public int? NormalizeCursorQuery(string? cursor = null);
 }
