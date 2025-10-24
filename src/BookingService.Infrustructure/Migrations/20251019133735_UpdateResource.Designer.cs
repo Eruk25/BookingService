ed826ea427b13dccc8088c091b5ce0732a873011
Danila <danilamatush72@gmail.com>
2025-10-24 12:03:53 +0300

    refactor(infrastructure): add migrations and update database

--- src/BookingService.Infrustructure/Migrations/20251019133735_UpdateResource.Designer.cs
+++ src/BookingService.Infrustructure/Migrations/20251019133735_UpdateResource.Designer.cs
@@ -4,6 +4,7 @@ using BookingService.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingService.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20251019133735_UpdateResource")]
+    partial class UpdateResource
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -62,8 +65,8 @@ namespace BookingService.Infrastructure.Migrations
 
                     b.Property<string>("Address")
                         .IsRequired()
-                        .HasMaxLength(500)
-                        .HasColumnType("nvarchar(500)");
+                        .HasMaxLength(100)
+                        .HasColumnType("nvarchar(100)");
 
                     b.Property<string>("Description")
                         .IsRequired()
---
+++ src/BookingService.Infrustructure/Migrations/20251019133735_UpdateResource.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateResource : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PricePerHour",
+                table: "Resources",
+                newName: "PricePerMonth");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Resources",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SourceId",
+                table: "Resources",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "SourceId",
+                table: "Resources");
+
+            migrationBuilder.RenameColumn(
+                name: "PricePerMonth",
+                table: "Resources",
+                newName: "PricePerHour");
+        }
+    }
+}
